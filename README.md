77d6c4308d0c28b939bc23abc60a530032f77165
rsr2425 <rsr2425@gmail.com>
2018-07-04 20:25:32 -0500

    Generated downloading script with full database of movies.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # movie_ratings
 The purpose of this project is to use the fastai library to test if deep learning image recognition techniques could be used to predict ratings on movies.
+
+Data obtained from https://grouplens.org/datasets/movielens/ as well as downloaded from Bing using their image API.
--- scripts/gen_dwnld_scrpt.py
+++ scripts/gen_dwnld_scrpt.py
@@ -3,12 +3,13 @@
 import numpy as np
 from clean import movies
 import os
+import sys
 
 movie_titles = list(movies['title'])
 
 file = open("download_data.sh", "w+")
 
-for i, movie in enumerate(movie_titles[:1000]):
+for i, movie in enumerate(movie_titles[:int(sys.argv[1])]):
     if i % 8 == 0 and i > 0:
         file.write("wait\n")
     file.write(f"python search_bing_api.py --query \"{movie}\" --output '' &\n")
